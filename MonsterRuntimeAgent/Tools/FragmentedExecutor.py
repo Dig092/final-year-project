68ac213407e73b61d97932c2b8ea729465bd3abf
Yash Bansal <yash114bansal@gmail.com>
2025-09-04 10:03:15 +0530

    Upgrade to add tot plan

--- MonsterRuntimeAgent/Tools/FragmentedExecutor.py
+++ MonsterRuntimeAgent/Tools/FragmentedExecutor.py
@@ -1,8 +1,8 @@
-
 from MonsterRuntimeAgent.Tools.Gemini import GeminiContentGenerator
 from MonsterRuntimeAgent.Tools.MultiStageExecutor import DataEngineer
-from MonsterRuntimeAgent.MonsterRuntimeCodeExecutor import MonsterRemoteCommandLineCodeExecutor
+from MonsterRuntimeAgent.Tools.ExperimentationModel import ExperimentPlanner
 from MonsterRuntimeAgent.Tools.RuntimeTools import MonsterNeoCodeRuntimeClient
+from MonsterRuntimeAgent.MonsterRuntimeCodeExecutor import MonsterRemoteCommandLineCodeExecutor
 
 EXAMPLE_INPUT = """
 # Task
@@ -85,8 +85,38 @@ class DataPrep():
         data_engineer = DataEngineer(problem_statement=self.data_prep_problem_statement,executor=self.monster_executor)
         return data_engineer.get_planner_summary()
 
+class ExperimentationRecord(BaseModel):
+    id: int = Field(description = "Incremental integer experiment ID increment with each experiment.")
+    experiment_problem_statement: str = Field(description = "Problem statement being tackled by this specific experiment.")
+    success: bool = Field(description = "True/False whether the iteration is successfull of failed")
+    experiment_outcome: str = Field(description = "Detailed outcome of experiment if experiment has succeeded, if failed error and possible resolution.")
+
+class ExperimentationController():
+    def __init__(self, problem_statement: str = EXAMPLE_INPUT):
+        #self.data_prep_handler = DataPrep(problem_statement = problem_statement)
+        self.tot_plan = self.create_tot_plan()
+        self.experimentation_list = []
+
+    def create_tot_plan(self):
+        planner =  ExperimentPlanner()
+        tot_plan = planner.plan_from_prompt(prompt=self.problem_statement)
+        return tot_plan
+
+    def get_next_experimentation_problem_statement(self):
+        upgraded_prompt = """
+        original problem statement: {self.problem_statement}
+
+        consider data prep and download has already happened
+
+        suggest a minimal first step experimentation problem statement:
+        solving dataloading running training if required for few minibatches 
+        optimize and find the maximum batch size for limited time experiment
+        Saving the output model
+        """
+
 
 
 if __name__ == "__main__":
-    dp = DataPrep()
+    #dp = DataPrep()
+    ec = ExperimentationController()
     import pdb;pdb.set_trace()
